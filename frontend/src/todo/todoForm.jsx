63d29e192b46c95dad1f785898e3322f85f5b39a
Tiago-devop <tiagodev@protonmail.com>
2021-01-13 11:56:43 -0800

    recommit

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
 import Grid from "../template/grid";
 import IconButton from "../template/iconButton";
+import { changeDescription } from "./todoActions";
 
-export default (props) => {
+const TodoForm = (props) => {
   const keyHandler = (e) => {
     if (e.key === "Enter") {
       e.shiftKey ? props.handleSearch() : props.handleAdd();
@@ -18,7 +22,7 @@ export default (props) => {
           id="description"
           className="form-control"
           placeholder="Adicione uma tarefa"
-          onChange={props.handleChange}
+          onChange={props.changeDescription}
           onKeyUp={keyHandler}
           value={props.description}
         ></input>
@@ -44,3 +48,8 @@ export default (props) => {
     </div>
   );
 };
+
+const mapStateToProps = (state) => ({ description: state.todo.description });
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ changeDescription }, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
